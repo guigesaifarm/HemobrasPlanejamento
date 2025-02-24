a836a91a8042d0974410e24c7857dcd57df07539
guigesaifarm <guigesaifarm@gmail.com>
2025-02-24 18:21:53 -0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,101 +1,101 @@
 const questions = [
     {
-        question: " O que é o Scrum?",
+        question: " O que é o Six Sigma?",
         answers: [
-            "A) Um tipo de reunião em metodologias ágeis.",
-            "B) Uma estrutura de trabalho para a gestão de projetos ágeis.",
-            "C) Um software de gerenciamento de projetos.",
-            "D) Um tipo de ferramenta de controle de qualidade."
+            "A) Uma metodologia de gestão de projetos ágeis.",
+            "B) Uma técnica de marketing para melhorar a satisfação do cliente.",
+            "C) Um conjunto de técnicas e ferramentas usadas para melhorar os processos e reduzir a variação.",
+            "D) Um método exclusivo para controlar custos em projetos."
+        ],
+        correct: 2
+    },
+    {
+        question: "Quais são as fases do ciclo <DMAIC> no Six Sigma?",
+        answers: [
+            "A) Definir, Medir, Analisar, Implantar e Controlar.",
+            "B) Definir, Medir, Analisar, Melhorar e Controlar.",
+            "C) Determinar, Mapear, Avaliar, Implantar e Controlar.",
+            "D) Diagnosticar, Medir, Analisar, Implementar e Controlar."
         ],
         correct: 1
     },
     {
-        question: "Qual dos seguintes papéis não faz parte da estrutura do Scrum?",
+        question: "Qual é o principal objetivo da fase <Definir> no ciclo DMAIC?",
         answers: [
-            "A) Scrum Master.",
-            "B) Product Owner.",
-            "C) Time de Desenvolvimento.",
-            "D) Gerente de Projetos."
+            "A) Analisar os dados para determinar a causa raiz do problema.",
+            "B) Definir os requisitos e escopo do projeto, incluindo os objetivos do cliente e do processo.",
+            "C) Implantar as soluções para os problemas identificados.",
+            "D) Controlar as melhorias implementadas."
+        ],
+        correct: 1
+    },
+    {
+        question: "Qual é a principal ferramenta usada na fase <Medir> do Six Sigma?",
+        answers: [
+            "A) Diagrama de Ishikawa.",
+            "B) Gráfico de controle.",
+            "C) Pareto Analysis.",
+            "D) Coleta de dados e análise de variabilidade do processo."
         ],
         correct: 3
     },
     {
-        question: "No Scrum, o que é o "Backlog" do Produto?",
+        question: "O que é o <Desvio Padrão> no contexto do Six Sigma?",
         answers: [
-            "A) Um conjunto de tarefas que o time de desenvolvimento precisa realizar",
-            "B) Um banco de dados de informações do projeto.",
-            "C)  A lista de requisitos e funcionalidades desejadas para o produto, priorizada pelo Product Owner.",
-            "D) A lista de todos os erros reportados durante o ciclo de desenvolvimento."
+            "A) A medida da média dos dados coletados.",
+            "B) A diferença entre o maior e o menor valor em um conjunto de dados.",
+            "C) A medida da dispersão ou variação dos dados em relação à média.",
+            "D) A soma de todos os valores em um conjunto de dados."
         ],
         correct: 2
     },
     {
-        question: "O que define a metodologia KANBAN em relação ao fluxo de trabalho? ",
+        question: "O que significa um <Nível Sigma> de 6 sigmas em um processo?",
         answers: [
-            "A)  Utiliza ciclos fixos e prazos definidos.",
-            "B)  É baseada em uma lista de tarefas pré estabelecidas.",
-            "C) Foca na visualização do trabalho e no controle do fluxo de tarefas em tempo real",
-            "D) Segue uma abordagem de desenvolvimento iterativo com entregas frequentes."
-        ],
-        correct: 2
-    },
-    {
-        question: "Em Scrum, qual é a duração típica de um Sprint?",
-        answers: [
-            "A) De 1 a 3 dias.",
-            "B) De 1 a 4 semanas.",
-            "C) De 1 a 6 meses.",
-            "D) De 3 a 12 meses."
+            "A)  O processo tem uma taxa de defeitos de 0,34%.",
+            "B) O processo tem um total de defeitos próximo de 0,01%.",
+            "C) O processo tem uma variação média de 6%.",
+            "D) O processo não tem variação."
         ],
         correct: 1
     },
     {
-        question: "Qual é o principal objetivo da cerimônia (Daily Scrum) no Scrum?",
+        question: "Na fase <Analisar> do DMAIC, qual é o principal objetivo?",
         answers: [
-            "A) Fazer uma reunião de planejamento do projeto.",
-            "B) Realizar a retrospectiva do projeto.",
-            "C) Atualizar o time sobre o progresso diário e identificar impedimentos.",
-            "D) Avaliar a entrega do produto."
-        ],
-        correct: 2
-    },
-    {
-        question: "Qual é a principal característica do Kanban em relação ao Scrum?",
-        answers: [
-            "A) Kanban utiliza sprints curtos e ciclos fixos.",
-            "B) Kanban não tem roles definidos como Scrum Master e Product Owner.",
-            "C) Kanban exige uma reunião diária obrigatória.",
-            "D) Kanban trabalha com prazos fixos de entrega."
+            "A) Medir a eficácia das soluções implementadas.",
+            "B) Identificar a causa raiz do problema ou da variação no processo.",
+            "C) Melhorar a eficiência do processo.",
+            "D) Controlar o novo processo após a implementação das melhorias."
         ],
         correct: 1
     },
     {
-        question: "O que envolve o processo de (Gerenciamento das Aquisições do Projeto)?",
+        question: "Qual é a função principal de um <Green Belt> no Six Sigma?",
         answers: [
-            "A) Determinar os riscos do projeto.",
-            "B) Identificar os stakeholders do projeto.",
-            "C) Planejar, realizar, monitorar e controlar as aquisições de bens e serviços externos.",
-            "D) Criar o cronograma do projeto."
-        ],
-        correct: 2
-    },
-    {
-        question: 'Qual das alternativas é uma das principais atividades do processo de (Planejamento das Aquisições)?',
-        answers: [
-            "A) Estabelecer os requisitos de cada tarefa no cronograma.",
-            "B) Definir os critérios para aquisição de recursos externos, como contratos e fornecedores.",
-            "C) Estabelecer as métricas de desempenho do time de desenvolvimento.",
-            "D) Criar o plano de comunicação do projeto."
+            "A) Liderar projetos de grande porte e definir a estratégia da organização.",
+            "B) Auxiliar na implementação de melhorias em processos, usando as ferramentas do Six Sigma, e atuar como facilitador de pequenos projetos.",
+            "C) Definir e controlar os níveis de produção da organização.",
+            "D) Supervisionar os projetos de todos os Black Belts na organização."
         ],
         correct: 1
     },
     {
-        question: "O que significa (Contratação de Aquisições) no ciclo de gerenciamento de aquisições de um projeto?",
+        question: "Qual é o propósito da ferramenta <Diagrama de Ishikawa> (ou Diagrama de Causa e Efeito)?",
         answers: [
-            "A) A fase onde são feitas as propostas para aquisição de bens ou serviços.",
-            "B) A fase onde o gerente de projetos assume a responsabilidade total pelas aquisições.",
-            "C) A fase onde são feitas as compras ou contratações de acordo com o planejamento.",
-            "D) A fase onde são definidas as funções dos fornecedores."
+            "A) Visualizar a variação no processo.",
+            "B) Identificar e categorizar as causas possíveis para um problema.",
+            "C) Controlar o fluxo de trabalho dentro de um processo.",
+            "D) Determinar a eficácia das ações corretivas."
+        ],
+        correct: 1
+    },
+    {
+        question: "Qual é o conceito de <Capacidade do Processo> (Cp) no Six Sigma?",
+        answers: [
+            "A) A capacidade de um processo para se adaptar rapidamente a mudanças.",
+            "B) A habilidade do processo em produzir o número máximo de unidades em um dado tempo.",
+            "C) A medida de quão bem um processo pode produzir resultados dentro de limites aceitáveis de especificação.",
+            "D) A quantidade de recursos necessários para operar um processo."
         ],
         correct: 2
     }
